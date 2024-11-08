001acc155e9c219a863c8be487f1eae18cfb99ff
dylan <dylan_brennan@yahoo.com>
2024-11-08 11:18:57 +0000

    feat: initial commit creating repo

---
+++ src/main/java/io/cleardocs/cleardocs/CleardocsApplication.java
@@ -0,0 +1,13 @@
+package io.cleardocs.cleardocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleardocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleardocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/cleardocs/cleardocs/CleardocsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.cleardocs.cleardocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleardocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
